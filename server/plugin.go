6afd37c083ded9255a03b79ea2cd6785dcd7c6d5
aswanth9495 <aswanth@interviewbit.com>
2020-09-05 05:26:38 +0530

    Add tracking to the plugin

--- server/plugin.go
+++ server/plugin.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -37,36 +35,36 @@ func (p *Plugin) modifyMessage(post *model.Post) (*model.Post, string) {
 	words := strings.Split(message, " ")
 	for _, word := range words {
 		if p.isChrTriggerWord(word) {
+			// Sending the Trigger message when chr trigger word has been detected
 			ephemeralPost := &model.Post{
 				UserId:    p.botID,
 				ChannelId: post.ChannelId,
-				Message:   "",
+				Props: model.StringInterface{
+					"attachments": []*model.SlackAttachment{
+						{
+							Text:       "Now, you can ask doubts for any concept and get them answered by getting on a **1-1 live call of ~ 15 minutes with the helper/TA**.",
+							Pretext:    "Seems like you have a Doubt! :thinking:. You can click on **Raise Concept Help Request** link below to raise a Concept help Request",
+							AuthorName: "Scaler",
+							AuthorLink: "https://www.scaler.com",
+							AuthorIcon: "https://assets.scaler.com/assets/academy/scalar-chat-icon-7dc8c6cce5bc388bd2ce9de1d347df05e5999d50d1b3a50ed910c93a97d97eca.png",
+							Title:      "Raise Concept Help Request",
+							TitleLink:  "http://www.scaler.com/academy/mentee-dashboard/mentee_help_request_dashboard/?ref=open-chr-modal",
+							ImageURL:   "https://assets.scaler.com/assets/academy/help_requests/bulb_question-57932b17f7273b95ad9b9bc23e8880437e35ad616927f09a1ad9c613372c5e18.png",
+						},
+					},
+				},
 			}
-			var props map[string]interface{}
-			data := `
-			{
-				"attachments": [
-					{
-						"fallback": "test",
-						"color": "#FF8000",
-						"pretext": "Seems like you have a Doubt! :thinking:. You can click on **Raise Concept Help Request** link below to raise a Concept help Request",
-						"text": "Now, you can ask doubts for any concept and get them answered by getting on a **1-1 live call of ~ 15 minutes with the helper/TA**.",
-						"author_name": "Scaler",
-						"author_icon": "https://assets.scaler.com/assets/academy/scalar-chat-icon-7dc8c6cce5bc388bd2ce9de1d347df05e5999d50d1b3a50ed910c93a97d97eca.png",
-						"author_link": "http://www.scaler.com",
-						"title": "Raise Concept Help Request",
-						"title_link": "http://www.scaler.com/academy/mentee-dashboard/mentee_help_request_dashboard/?ref=open-chr-modal",
-						"image_url": "https://assets.scaler.com/assets/academy/help_requests/bulb_question-57932b17f7273b95ad9b9bc23e8880437e35ad616927f09a1ad9c613372c5e18.png"
-					}
-				]
-			}`
-			err := json.Unmarshal([]byte(data), &props)
-			if err != nil {
-				fmt.Println("Failed to create the props")
-			}
-			ephemeralPost.SetProps(props)
-
 			p.API.SendEphemeralPost(post.UserId, ephemeralPost)
+			// Tracking trigger coount start
+			data, error := p.API.KVGet("chr_trigger_count")
+			if error != nil {
+				p.API.KVSet("chr_triggers", []byte("0"))
+			} else {
+				triggerCount, _ := strconv.Atoi(string(data))
+				triggerCount++
+				p.API.KVSet("chr_triggers", []byte(strconv.Itoa(triggerCount)))
+			}
+			// Tracking trigger coount end
 			return post, ""
 		}
 	}
@@ -80,10 +78,3 @@ func (p *Plugin) MessageWillBePosted(_ *plugin.Context, post *model.Post) (*mode
 func (p *Plugin) MessageWillBeUpdated(_ *plugin.Context, post *model.Post) (*model.Post, string) {
 	return p.modifyMessage(post)
 }
-
-// ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
-func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
-}
-
-// See https://developers.mattermost.com/extend/plugins/server/reference/
