21891e5880afe8f815ca658050511a79abe7eb1e
aswanth9495 <aswanth@interviewbit.com>
2020-09-09 02:25:56 +0530

    Add datewise tracking to plugin

--- server/plugin.go
+++ server/plugin.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"math/rand"
 	"regexp"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/plugin"
@@ -53,10 +55,9 @@ func (p *Plugin) modifyMessage(post *model.Post) (*model.Post, string) {
 				Props: model.StringInterface{
 					"attachments": []*model.SlackAttachment{
 						{
-							Text:       "Now, you can ask doubts for any concept and get them answered by getting on a **1-1 live call of ~ 15 minutes with the helper/TA**.",
-							Pretext:    "Seems like you have a Doubt! :thinking:. You can click on **Raise Concept Help Request** link below to raise a Concept help Request",
+							Text:       "Now, you can ask doubts for any concept and get them answered by getting on a ***1-1 live call of ~ 15 minutes with a helper/TA***.",
+							Pretext:    "##### Seems like you have a Doubt! :thinking:. You can click on Raise Concept Help Request link below to get on call :telephone_receiver: with a TA ",
 							AuthorName: "Scaler",
-							AuthorLink: "https://www.scaler.com",
 							AuthorIcon: "https://assets.scaler.com/assets/academy/scalar-chat-icon-7dc8c6cce5bc388bd2ce9de1d347df05e5999d50d1b3a50ed910c93a97d97eca.png",
 							Title:      "Raise Concept Help Request",
 							TitleLink:  "http://www.scaler.com/academy/mentee-dashboard/mentee_help_request_dashboard/?ref=open-chr-modal",
@@ -70,16 +71,38 @@ func (p *Plugin) modifyMessage(post *model.Post) (*model.Post, string) {
 			// Tracking for no of times we've sent the trigger message
 			p.IncrementTrackingCount("chr_message_trigger_count")
 			// Tracking for the user the the trigger message have been sent to
-			postedUserName, _ := p.API.GetUser(post.UserId)
-			p.IncrementTrackingCount("chr_trigr_usr_" + postedUserName.Username)
+			postedUser, _ := p.API.GetUser(post.UserId)
+			p.IncrementTrackingCount("chr_trigr_usr_" + postedUser.Username)
 			// Tracking for the channel the trigger message have been sent to
 			p.IncrementTrackingCount("chr_trigr_chnl_" + post.ChannelId)
-
 			postedChannel, err := p.API.GetChannel(post.ChannelId)
 			if postedChannel.TeamId != "" && err == nil {
 				// Tracking for the team the trigger message have been sent to
 				p.IncrementTrackingCount("chr_trigr_team_" + postedChannel.TeamId)
 			}
+			// Date wise Tracking for the post
+			currentTime := time.Now().Local()
+			postList, err := p.API.KVGet("chr_trgr_date_" + currentTime.Format("2006-01-02"))
+			if postList == nil {
+				newPostList := []model.StringInterface{{
+					"post":         post,
+					"channel_name": postedChannel.Name,
+					"user":         postedUser.Username,
+				}}
+				newPostListJSON, _ := json.Marshal(newPostList)
+				p.API.KVSet("chr_trgr_date_"+currentTime.Format("2006-01-02"), newPostListJSON)
+			} else {
+				newPostList := []model.StringInterface{}
+				json.Unmarshal(postList, &newPostList)
+				newPostList = append(newPostList, model.StringInterface{
+					"post":         post,
+					"channel_name": postedChannel.Name,
+					"user":         postedUser.Username,
+				})
+				newPostListJSON, _ := json.Marshal(newPostList)
+				p.API.KVSet("chr_trgr_date_"+currentTime.Format("2006-01-02"), newPostListJSON)
+			}
+
 			return post, ""
 		}
 		return post, ""
